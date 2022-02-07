4504aa5fc3d46798337a44af2a93ab62aa948024
brice <briceunintendedfraud@fastmail.com>
2022-02-07 09:39:21 +0100

    add scripts

---
+++ install.sh
@@ -0,0 +1,6 @@
+cp alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+cp zsh/.zshrc ~/.zshrc
+
+cp i3/config ~/.config/i3/config
+cp i3/i3blocks.conf ~/.config/i3/i3blocks.conf
---
+++ update.sh
@@ -0,0 +1,6 @@
+cp ~/.config/alacritty/alacritty.yml alacritty/alacritty.yml
+
+cp ~/.zshrc zsh/.zshrc
+
+cp ~/.config/i3/config i3/config
+cp ~/.config/i3/i3blocks.conf i3/i3blocks.conf
