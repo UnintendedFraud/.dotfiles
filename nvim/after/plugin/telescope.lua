2f42337fe48f6d0d6707063c0082b6544bee5a03
Shantanu Raj <s@sraj.me>
2023-05-09 15:43:36 +0200

    feat(nvim): Use ripgrep for fuzzy find files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,21 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+local builtin = require("telescope.builtin")
+
+local M = {}
+
+M.project_files = function()
+	local opts = require("telescope.themes").get_ivy({})
+	opts.find_command = { "rg", "--files", "--hidden", "--follow", "--glob", "!.git/*" }
+	vim.fn.system("git rev-parse --is-inside-work-tree")
+	if vim.v.shell_error == 0 then
+		builtin.git_files(opts)
+	else
+		opts.hidden = true
+		builtin.find_files(opts)
+	end
+end
+
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p", M.project_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
