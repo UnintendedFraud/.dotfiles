92037e846b9ac696786348babd3250681145c1b3
unintendedfraud <unintendedfraud@fastmail.com>
2024-02-03 14:54:42 +0100

    fix autoformat

--- nvim/nvim/lua/mimu/init.lua
+++ nvim/nvim/lua/mimu/init.lua
@@ -7,21 +7,21 @@ local MimuGroup = augroup("mimu", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 autocmd("LspAttach", {
     group = MimuGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
-	    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-	    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	    vim.keymap.set("n", "<leader>vd", function() vim.lsp.diagnostic.open_float() end, opts)
-	    -- vim.keymap.set("n", "[d", function() vim.lsp.diagnostic.goto_next() end, opts)
-	    -- vim.keymap.set("n", "]d", function() vim.lsp.diagnostic.goto_prev() end, opts)
-	    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.lsp.diagnostic.open_float() end, opts)
+        -- vim.keymap.set("n", "[d", function() vim.lsp.diagnostic.goto_next() end, opts)
+        -- vim.keymap.set("n", "]d", function() vim.lsp.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end
 })
